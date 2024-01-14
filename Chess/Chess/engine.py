f4f40749b9f993939fe9ba741b871cc3df3903e3
kunxingg <150636707+kunxingg@users.noreply.github.com>
2024-01-13 22:28:58 -0500

    Add files via upload

---
+++ Chess/Chess/engine.py
@@ -0,0 +1,16 @@
+class GameState():
+    def __init__(self):
+        #board is 2d list, first char is black or white, second char is pieve type
+        self.board = [
+            ["bR", "bN", "bB", "bQ", "bK", "bB", "bN", "bR"],
+            ["bp", "bp", "bp", "bp", "bp", "bp", "bp", "bp"],
+            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["wp", "wp", "wp", "wp", "wp", "wp", "wp", "wp"],
+            ["wR", "wN", "wB", "wQ", "wK", "wB", "wN", "wR"]]
+        self.whiteToMove = True
+        self.movelog= []
+        
+    
